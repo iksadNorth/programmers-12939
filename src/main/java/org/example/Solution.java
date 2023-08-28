6c8ef05c1024d0e6f9de7b43b00e92dfaea1f590
iksadNorth <iksadNorth@gmail.com>
2023-08-28 09:33:12 +0900

    문제 풀이 및 테스트 통과

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        int max=-Integer.MAX_VALUE, min=Integer.MAX_VALUE;
+        for(String i : s.split(" ")) {
+            int j = Integer.parseInt(i);
+
+            if(j > max) max = j;
+            if(j < min) min = j;
+        }
+        return String.format("%s %s", min, max);
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +15,31 @@ class SolutionTest {
         Assertions.assertEquals(result, answer);
     }
 
-    @Disabled
-    @Test @DisplayName("이 메서드는 Mock Up 이다.")
-    void testcase0() {
+    @Test @DisplayName("Test 1")
+    void testcase1() {
         // given
         String s = "1 2 3 4";
         String answer = "1 4";
+
+        // when & then
+        assertAnswer(s, answer);
+    }
+
+    @Test @DisplayName("Test 2")
+    void testcase2() {
+        // given
+        String s = "-1 -2 -3 -4";
+        String answer = "-4 -1";
+
+        // when & then
+        assertAnswer(s, answer);
+    }
+
+    @Test @DisplayName("Test 3")
+    void testcase3() {
+        // given
+        String s = "-1 -1";
+        String answer = "-1 -1";
 
         // when & then
         assertAnswer(s, answer);
